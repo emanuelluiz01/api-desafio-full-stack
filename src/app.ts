22e31fd99ed68508b4e702b040ca7faec8a877e2
emanuelluiz01 <emanuelluizdea@gmail.com>
2023-06-02 19:04:45 -0300

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ import { loginRoute } from "./routes/login.routes";
 const app: Application = express();
 app.use(express.json());
 
-app.use("/users", usersRoutes);
 app.use(cors());
+app.use("/users", usersRoutes);
 app.use("/contacts", contactsRoutes);
 app.use("/login", loginRoute);
 
